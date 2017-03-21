cf8e6f0c32d70da9e29a93c6ce3f40b7f64ff65d
Nick-Brennan <nick_p_b@yahoo.com>
2017-03-20 22:27:54 -0700

    basic routes for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var express = require('express'),
 *******|| to use EJS first npm install -s ejs ||*************
 *************|| then uncomment below ||*********************/
 
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use("/public", express.static("public"));
 app.use("/vendor", express.static("bower_components"));
@@ -20,6 +20,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //TEST ENDPOINT
 app.get('/hw', ctrl.testF);
+
+app.get('/', function(req, res){
+  res.cookie('test', 'test cookie');
+  res.render('index', {test: "hello world"});
+});
+
+app.post('/', function(req, res){
+  res.sendStatus(200);
+});
 
 http.listen(process.env.PORT || 3000, function(){
   console.log("Application listening on port: " + (process.env.PORT || 3000));
