6027d8dd3ed5baee95f6866a8870578645dd4c79
Nick-Brennan <nick_p_b@yahoo.com>
2017-03-20 23:26:25 -0700

    protecting the routes

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ var express = require('express'),
     path = require('path'),
     db = require('./models'),
     request = require('request'),
-    ctrl = require('./controllers');
-
-/*********|| I haven't included a view engine ||*************
-*******|| to use EJS first npm install -s ejs ||*************
-*************|| then uncomment below ||*********************/
-var jwt = require('jwt-simple');
+    ctrl = require('./controllers'),
+    jwt = require('jwt-simple');
 
 app.set('view engine', 'ejs');
 
@@ -23,19 +19,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/hw', ctrl.testF);
 
 app.get('/', function(req, res){
-  res.cookie('test', 'test cookie');
-  res.render('index', {test: "hello world"});
+  if(req.cookies.jwt){
+    res.render('index', {test: "hello world"});
+  }else{
+    res.sendStatus(404);
+  }
+  
 });
 
-app.post('/', function(req, res){
-  
+app.post('/', function(req, res){ 
   var encodedJWT = req.body.jwt;
   console.log(encodedJWT);
   var secret = "frgome21hqhxumhcesuupjnrfz2ban0br0u4eqmnnxgggdkzlod2v4tnz5q2ovbx5wlzayz2smqgtoiqfnk0nn3rqnkrokjqqxw52a4f4dm1ipgjrdi0l3hh1labjzayodcxa55ss0qurvxo4whsecpguu1bcfe2v00cygwicwix035hytiqns0rg43wa5p0ustd1yl1lo0dbo5c5ziwbmcgr5hxaubgd1m1oakr0w4lyqkubiuzykrlzolujam";
-  var decodedJWT = jwt.decode(encodedJWT, secret); 
-  res.cookie('token', req.body.jwt)
-  res.render('index', {test: JSON.stringify(decodedJWT)});
-  // res.sendStatus(200);
+  var decodedJWT = jwt.decode(encodedJWT, secret);
+  if(decodedJWT){
+    res.cookie('token', req.body.jwt)
+    res.render('index', {test: JSON.stringify(decodedJWT)});
+  }else{
+    res.sendStatus(404);
+  }
 });
 
 http.listen(process.env.PORT || 3000, function(){
